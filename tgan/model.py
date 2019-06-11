6ceba16b86628e4f99a5b744eca7121769b6ffab
Bauke <bauke.brenninkmeijer@gmail.com>
2019-06-11 15:16:50 +0200

    add dataset logging

--- tgan/model.py
+++ tgan/model.py
@@ -598,7 +598,7 @@ class TGANModel:
         self, continuous_columns, output='output', gpu=None, max_epoch=5, steps_per_epoch=10000,
         save_checkpoints=True, restore_session=True, batch_size=200, z_dim=200, noise=0.2,
         l2norm=0.00001, learning_rate=0.001, num_gen_rnn=100, num_gen_feature=100,
-        num_dis_layers=1, num_dis_hidden=100, optimizer='AdamOptimizer', comet_ml_key=None
+        num_dis_layers=1, num_dis_hidden=100, optimizer='AdamOptimizer', comet_ml_key=None, ds=None
     ):
         """Initialize object."""
         # Output
@@ -631,6 +631,8 @@ class TGANModel:
 
         self.comet_ml_key = comet_ml_key
         self.experiment = Experiment(api_key=comet_ml_key, project_name='tgan-wgan-gp', workspace="baukebrenninkmeijer")
+        if ds:
+            self.experiment.log_dataset_info(ds)
         self.gpu = gpu
 
     def get_model(self, training=True):
