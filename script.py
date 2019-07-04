599a3be7d0a6f2bc3b4ff66c0826c7e585d07ae3
Bauke <bauke.brenninkmeijer@gmail.com>
2019-07-04 13:54:01 +0200

    Remove travis, add script, update model to handle cometml

---
+++ script.py
@@ -0,0 +1,64 @@
+from comet_ml import Experiment
+import pandas as pd
+from tgan.model import TGANModel
+import argparse
+
+
+def get_data(ds, drop=None, n_unique=20, sep=';', suffix='cat'):
+    d = pd.read_csv(f'../data/{ds}/{ds}_{suffix}.csv', sep=sep)
+    if drop is not None:
+        d = d.drop(drop, axis=1)
+
+    continuous_columns = []
+    for col in d._get_numeric_data().columns:
+        if len(d[col].unique()) > n_unique:
+            continuous_columns.append(d.columns.get_loc(col))
+    return d, continuous_columns
+
+
+parser = argparse.ArgumentParser(description='Evaluate data synthesizers')
+parser.add_argument('--dataset', nargs='*', help='Which dataset to choose. Options are berka, creditcard and ticket', default=['berka', 'census', 'creditcard'])
+
+args = parser.parse_args()
+datasets = args.dataset
+
+for ds in datasets:
+
+    if ds == 'berka':
+        d, continuous_columns = get_data(ds, drop=['trans_bank_partner', 'trans_account_partner'])
+    elif ds == 'census':
+        d, continuous_columns = get_data(ds, sep=',')
+    elif ds == 'creditcard':
+        d, continuous_columns = get_data(ds, sep=',', suffix='num')
+    else:
+        raise Exception('Unknown dataset mentioned')
+
+    project_name = "tgan-skip-connections"
+    experiment = Experiment(api_key="49HGMPyIKjokHwg2pVOKWTG67",
+                            project_name=project_name, workspace="baukebrenninkmeijer")
+
+    tgan = TGANModel(continuous_columns,
+                     restore_session=False,
+                     max_epoch=100,
+                     steps_per_epoch=5000,
+                     batch_size=200,
+                     experiment=experiment,
+                     num_gen_rnn=50,
+                     num_gen_feature=64)
+    tgan.fit(d)
+
+    model_path = f'model/{ds}_{project_name}'
+
+    num_samples = 100000
+    new_samples = tgan.sample(num_samples)
+
+    p = new_samples.copy()
+    if ds == 'berka' or ds == 'census':
+        p[p._get_numeric_data().columns] = p[p._get_numeric_data().columns].astype('int')
+    if ds == 'creditcard':
+        p[['time', 'class']] = p[['time', 'class']].astype('int')
+    p.to_csv(f'samples/{ds}_sample_{project_name}_2layerskip.csv', index=False)
+    experiment.log_dataset_info(name=ds)
+    experiment.end()
+
+    tgan.save(model_path, force=True)
--- tgan/model.py
+++ tgan/model.py
@@ -15,6 +15,8 @@ import pickle
 import tarfile
 
 import numpy as np
+from comet_ml import Experiment
+from tensorpack.callbacks import CometMLMonitor, MergeAllSummaries
 import tensorflow as tf
 from tensorpack import (
     BatchData, BatchNorm, Dropout, FullyConnected, InputDesc, ModelDescBase, ModelSaver,
@@ -596,7 +598,7 @@ class TGANModel:
         self, continuous_columns, output='output', gpu=None, max_epoch=5, steps_per_epoch=10000,
         save_checkpoints=True, restore_session=True, batch_size=200, z_dim=200, noise=0.2,
         l2norm=0.00001, learning_rate=0.001, num_gen_rnn=100, num_gen_feature=100,
-        num_dis_layers=1, num_dis_hidden=100, optimizer='AdamOptimizer',
+        num_dis_layers=1, num_dis_hidden=100, optimizer='AdamOptimizer', comet_ml_key=None, experiment=None, ds=None
     ):
         """Initialize object."""
         # Output
@@ -627,6 +629,13 @@ class TGANModel:
         if gpu:
             os.environ['CUDA_VISIBLE_DEVICES'] = gpu
 
+        if experiment is not None:
+            self.experiment = experiment
+        elif comet_ml_key is not None:
+            self.comet_ml_key = comet_ml_key
+            self.experiment = Experiment(api_key=comet_ml_key, project_name='tgan-wgan-gp', workspace="baukebrenninkmeijer")
+        if ds is not None:
+            experiment.log_dataset_info(name=ds)
         self.gpu = gpu
 
     def get_model(self, training=True):
@@ -702,14 +711,20 @@ class TGANModel:
             starting_epoch = 1
 
         action = 'k' if self.restore_session else None
-        logger.set_logger_dir(self.log_dir, action=action)
+        # logger.set_logger_dir(self.log_dir, action=action)
 
         callbacks = []
+        monitors = []
         if self.save_checkpoints:
             callbacks.append(ModelSaver(checkpoint_dir=self.model_dir))
+        callbacks.append(MergeAllSummaries(period=10))
+
+        if self.experiment is not None:
+            monitors.append(CometMLMonitor(experiment=self.experiment))
 
         trainer.train_with_defaults(
             callbacks=callbacks,
+            monitors=monitors,
             steps_per_epoch=self.steps_per_epoch,
             max_epoch=self.max_epoch,
             session_init=session_init,
