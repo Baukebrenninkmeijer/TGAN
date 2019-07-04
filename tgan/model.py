4ea44b3c5b4072063783a21cd53af4fba2a1e818
Bauke <bauke.brenninkmeijer@gmail.com>
2019-07-04 13:46:59 +0200

    kutconflict

--- tgan/model.py
+++ tgan/model.py
@@ -716,7 +716,7 @@ class TGANModel:
             starting_epoch = 1
 
         action = 'k' if self.restore_session else None
-#         logger.set_logger_dir(self.log_dir, action=action)
+        # logger.set_logger_dir(self.log_dir, action=action)
 
         callbacks = []
         monitors = []
